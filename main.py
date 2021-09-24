d0d2cddfeea7081277d35d89d21b3e6f07793a0e
nitaca <90216107+nitaca@users.noreply.github.com>
2021-09-24 20:40:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# This lines offers a greeting to the user
+print("Hello! Welcome to the Band Name Generator!")
+
+# This lines is going to have as an output the city the user grew up in
+city = input("What city did you grow up in?\n")
+
+# This line is going to have as an output the user's pet's name
+name = input("What is your pet's name?\n")
+
+# This line is going to combine the city and the pet name and is going to generate the band name
+print("Your band name could be " + city + " " + name)
